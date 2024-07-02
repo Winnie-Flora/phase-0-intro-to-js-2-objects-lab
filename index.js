3d4a51b552f3fa8bc2f719e2a64c1ec1001723f0
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-07-02 13:24:02 +0300

    Adds objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee={
+    name: "Alex",
+    streetAddress: "1002",
+};
+
+function updateEmployeeWithKeyAndValue(employee,name,streetAddress){
+    const copyEmployee={...employee};
+    copyEmployee.name="Sam";
+    copyEmployee.streetAddress="11 Broadway";
+    return copyEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,streetAddress){
+    employee.name="Sam";
+    employee.streetAddress="12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    let newEmployee={...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
